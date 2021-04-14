82c7ee8b35c22bc67e13c3d02c4d22dea22b8081
Sethulakshmibabu <sethulakshmib21@gmail.com>
2021-04-14 19:27:53 +0530

    Sethu update the task

--- src/test/java/org/method/FirstMethod.java
+++ src/test/java/org/method/FirstMethod.java
@@ -10,4 +10,13 @@ public class FirstMethod {
 	private void method3() {
 		System.out.println("method3");
 	}
+	private void method4() {
+		System.out.println("method4");
+	}
+	private void method5() {
+		System.out.println("method5");
+	}
+	private void method6() {
+		System.out.println("method6");
+	}
 }
